dbcbb57459939753c957c4ba9e3ae404819e8961
ddarahakit <ddarahakit@gmail.com>
2026-04-19 10:39:01 +0900

    [feat] 상세 페이지 백엔드 연동

--- frontend/src/api/placeApi.js
+++ frontend/src/api/placeApi.js
@@ -4,4 +4,8 @@ export const placeApi = {
   /** 목록 조회 */
   getList: () =>
     $axios.get('/place/all').then(res => res.data.results).catch((error) => {error.data}),
+
+  /** 장소 기준 상세 조회 (부모 컨텐츠 + 전체 장소 반환) */
+  getDetail: (id) =>
+    $axios.get(`/place/${id}`).then(res => res.data.results).catch((error) => {error.data}),
 };
